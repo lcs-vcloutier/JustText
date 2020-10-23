4e3aa972b3bb0ddce299ac3c35b6aeeba2abd9b9
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-23 11:39:37 -0400

    Initial Commit

---
+++ JustText/JustTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustTextApp.swift
+//  JustText
+//
+//  Created by Cloutier, Vincent on 2020-10-23.
+//
+
+import SwiftUI
+
+@main
+struct JustTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
